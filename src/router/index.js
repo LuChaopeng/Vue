602defc5dba69c3cb181bfb3c263a4d722566977
luchaopeng <chaopeng.lu@qq.com>
2020-12-11 16:43:21 +0800

    1211tem

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HW from '@/components/HelloWorld'    /*这个名字可以自定义 */
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/url',   //浏览器url
-      name: 'HellWorld',  //内部使用的名字，暂时不用
-      component: HelloWorld //对应的vue页面
+      path: '/url',   /*浏览器url*/
+      name: 'HellWorld',  /*内部使用的名字，暂时不用*/
+      component: HW /*对应的vue页面*/
     }
   ]
 })
